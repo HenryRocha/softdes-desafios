731be34193ccea464f4262b90d8ecd7051dddab5
Henry Rocha <henryrocha@protonmail.com>
2021-08-31 19:21:03 -0300

    Modulo adduser passando no pylint

--- src/adduser.py
+++ src/adduser.py
@@ -1,18 +1,44 @@
+"""
+Esse modulo é usado para criar todos os usuários, a partir do arquivo .csv,
+e inserio-los no banco de dados.
+"""
+
+
 import sqlite3
 import hashlib
 
-def addUser(user, pwd, type):
-    conn = sqlite3.connect('quiz.db')
+
+def add_user(user, pwd, user_type):
+    """
+    Insire um usuário no banco de dados.
+    """
+
+    conn = sqlite3.connect("quiz.db")
     cursor = conn.cursor()
-    cursor.execute('Insert into USER(user,pass,type) values("{0}","{1}","{2}");'.format(user, pwd, type))
+    cursor.execute(
+        'Insert into USER(user,pass,type) values("{0}","{1}","{2}");'.format(
+            user, pwd, user_type
+        )
+    )
     conn.commit()
-    conn.close()  
+    conn.close()
 
-with open('users.csv','r') as file:
-  lines = file.read().splitlines()
 
-for users in lines:
-  (user, type) = users.split(',')
-  print(user)
-  print(type)
-  addUser(user, hashlib.md5(user.encode()).hexdigest(), type)
+def main():
+    """
+    Função principal do script.
+    Lê o arquivo .csv e insire os usuários no banco de dados.
+    """
+
+    with open("users.csv", "r", encoding="utf-8") as file:
+        lines = file.read().splitlines()
+
+    for users in lines:
+        (user, user_type) = users.split(",")
+        print(user)
+        print(user_type)
+        add_user(user, hashlib.md5(user.encode()).hexdigest(), user_type)
+
+
+if __name__ == "__main__":
+    main()
