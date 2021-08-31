f72e421875c66b8ec159227a903d2e565272a8e6
Henry Rocha <henryrocha@protonmail.com>
2021-08-31 19:21:16 -0300

    Modulo desafio passando no pylint

--- src/desafio.py
+++ src/desafio.py
@@ -1,3 +1,11 @@
-def desafio1(n):
-    #return 0
-    return n
+"""
+Implementação do desafio 1.
+"""
+
+
+def desafio1(_n):
+    """
+    Implementação do desafio 1. Esse desafio consiste em criar uma função
+    que sempre retorna 0.
+    """
+    return 0
